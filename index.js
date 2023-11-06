de05e6a0e8a0928cc865d9248257d12e4b1ad7b8
ShaplaRani <shaplarani621@gmail.com>
2023-11-06 16:55:46 +0600

    added the deleted option

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,6 +29,13 @@ async function run() {
     // await client.connect();
     const productCollection = client.db('bitJobs').collection('product');
     // const bookingCollection = client.db('carDoctor').collection('bookings');
+
+  //   app.get("/api/product", async(req, res)=> { 
+  //     const cursor = productCollection.find();
+  //   const result = await cursor.toArray()
+  //   res.send(result)
+  // })
+
     app.get('/api/category-product', async(req, res) => {
       let queryObj = {};
       const category = req.query.category;
@@ -51,11 +58,56 @@ async function run() {
       res.send(result);
     })
 
+    //single data get
+    app.get("/api/email-product/:id", async(req, res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const query= {_id: new ObjectId(id)}
+      console.log(query);
+      const result = await productCollection.findOne(query)
+      console.log(result);
+      res.send(result)
+
+  })
+
     app.post('/api/user/create-product', async(req, res) => {
       const product = req.body; console.log(product);
       const result = await productCollection.insertOne(product);
       res.send(result);
   })
+
+  app.put('/api/email-product/:id', async(req, res) => {
+    const id =  req.params.id;
+    console.log(id);
+        // const product = req.body;
+        // const filter= {_id: new ObjectId(id)}
+        // const option = {upsert: true}
+        // const updateProduct = {
+        //  $set: {
+        //    photo: product.photo,
+        //    name:product.name,
+        //    brand:product.brand,
+        //    type:product.type,
+        //    price:product.price,
+        //    description:product.description,
+        //    rating:product.rating,
+           
+        //  }
+        // }
+        // const result = await automotiveCollection.updateOne(filter, updateProduct, option)
+        // res.send(result)
+  })
+
+  app.delete("/api/email-product/:id", async(req, res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const query= {_id: new ObjectId(id)}
+    console.log(query);
+    const result = await productCollection.deleteOne(query)
+    console.log(result);
+    res.send(result)
+
+})
 
 
     // Send a ping to confirm a successful connection

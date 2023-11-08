27e57b5b27a137d0d91edf4309acb9a182992363
ShaplaRani <shaplarani621@gmail.com>
2023-11-08 17:23:34 +0600

    added the patch method

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-//const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-
+//https://online-marketplaces-server.vercel.app
+//https://online-marketplaces-server.vercel.app
 //middleware
 app.use(cors({
   origin:[
     'http://localhost:5174',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://online-marketplace-4abba.web.app',
+    'https://online-marketplace-4abba.firebaseapp.com'
   ],
   credentials:true
 }));
 app.use(express.json());
-
+app.use(cookieParser());
 
 
 
@@ -31,6 +34,25 @@ const client = new MongoClient(uri, {
   }
 });
 
+//middleware
+const verifyToken = async(req, res, next) => {
+  const token = req?.cookies?.token;
+  //console.log(' token  in the middleware', token);
+  if(!token){
+      return res.status(401).send({message: 'unauthorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+      }
+      //if token is valid then it would be decoded
+      // console.log('value in the token', decoded);
+        req.user = decoded;
+      next();
+  })
+  
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -43,7 +65,7 @@ async function run() {
   //jwt related api
   app.post('/jwt', async(req, res) => {
     const user = req.body;
-    console.log('user for token', user);
+   // console.log('user for token', user);
     //token create
      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
     // console.log('token', token);
@@ -62,6 +84,21 @@ async function run() {
     .send({success: true})
     //res.send({token})
  })
+    //token delete
+     app.post('/logout', async( req, res) =>{
+     const user = req.body;
+    //console.log("logging out", user);
+     
+     res.clearCookie('token', {maxAge: 0}).send({success: true})
+  //     res.clearCookie(
+  //     "token",
+  //     {
+  //         maxAge: 0,
+  //         secure: process.env.NODE_ENV === "production" ? true: false,
+  //         sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  //     }
+  // )
+})
 
     //job related api
     app.get('/api/category-product', async(req, res) => {
@@ -75,9 +112,14 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/api/email-product', async(req, res) => {
-      let queryObj = {};
+    app.get('/api/email-product',verifyToken, async(req, res) => {
+     
+      console.log('token owner',req.user);
       const email = req.query.email;
+      if(req.user.email !== email){
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      let queryObj = {};
       if(email ){
         queryObj.email  = email ;
       }
@@ -136,19 +178,41 @@ async function run() {
 
 //bit job relate api
 //http://localhost:5000/api/user-email?email=shaplarani621@gmail.com
-app.get('/api/user-email', async(req, res) => {
-  let queryObj = {};
+//http://localhost:5000/api/user-email?sortField=status&sortOrder=asc
+app.get('/api/user-email',verifyToken, async(req, res) => {
+  
   const email = req.query.email;
+
+  if(req.user.email !== email){
+    return res.status(403).send({ message: 'forbidden access' })
+  }
+
+  let queryObj = {};
+  //sorting
+  const sortObj ={};
+  const sortField = req.query.sortField;
+  const sortOrder = req.query.sortOrder;
+  if(sortField && sortOrder){
+    sortObj[sortField] = sortOrder
+  }
+  //
   if(email ){
     queryObj.userEmail  = email ;
   }
-  const cursor = jobCollection.find(queryObj);
+
+  const cursor = jobCollection.find(queryObj).sort(sortObj);
   const result = await cursor.toArray();
   res.send(result);
 })
-app.get('/api/buyer-email', async(req, res) => {
-  let queryObj = {};
+app.get('/api/buyer-email',verifyToken, async(req, res) => {
+  
   const email = req.query.email;
+
+  if(req.user.email !== email){
+    return res.status(403).send({ message: 'forbidden access' })
+  }
+
+  let queryObj = {};
   if(email ){
     queryObj.buyerEmail  = email ;
   }
@@ -162,15 +226,32 @@ app.get('/api/buyer-email', async(req, res) => {
     const result = await jobCollection.insertOne(product);
      res.send(result);
 })
-
-app.patch('/api/buyer-email/:id', async(req, res) => {
+//update accept
+app.put('/api/update-status/:id', async(req, res) => {
   const id = req.params.id;
   const filter = {_id : new ObjectId(id)}
   const updateJob = req.body;
   console.log(updateJob);
   const updateDoc = {
       $set : {
-          status: updateJob.status
+          status: updateJob.status,
+          request:updateJob.request 
+      }
+  }
+
+  const result = await  jobCollection.updateOne(filter, updateDoc);
+  console.log(result);
+  res.send(result);
+}) 
+//update complete
+app.patch('/api/delete-complete/:id', async(req, res) => {
+  const id = req.params.id;
+  const filter = {_id : new ObjectId(id)}
+  const updateJob = req.body;
+  console.log(updateJob);
+  const updateDoc = {
+      $set : {
+        complete: updateJob.complete
       }
   }
 
